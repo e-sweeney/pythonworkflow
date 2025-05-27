a18c5ecc5abbf87311c84b54eb6dcee69346a90c
edwina sweeney <e-sweeney@users.noreply.github.com>
2025-05-27 12:19:17 +0100

    added john

--- ModelCleaning/train_test_dataset.py
+++ ModelCleaning/train_test_dataset.py
@@ -30,7 +30,7 @@ print(df.head())
 
 
 
-X= df["Age"].values.reshape(10,1)
+X= df["Age"].values.reshape(11,1)
 y= df["Salary"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
